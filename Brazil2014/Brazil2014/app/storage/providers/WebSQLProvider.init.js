14bbd41e21da58db1ebff32824e383c24ea3f6a8
krux <krux@krux.fr>
2014-06-16 23:15:40 +0200

    Modificaitons du modeles pour intégration prochaine

--- Brazil2014/Brazil2014/app/storage/providers/WebSQLProvider.init.js
+++ Brazil2014/Brazil2014/app/storage/providers/WebSQLProvider.init.js
@@ -23,9 +23,9 @@
             //Creation de la structure
             tx.executeSql("CREATE TABLE IF NOT EXISTS Team(ID INTEGER unique, Libelle TEXT, [Group] TEXT, IdFIFA TEXT);")
             tx.executeSql("CREATE TABLE IF NOT EXISTS Stade(ID INTEGER unique, Libelle TEXT, City TEXT, Capacity INTEGER);")
-            tx.executeSql("CREATE TABLE IF NOT EXISTS Match(ID INTEGER unique, Date DATETIME, Hour TIME, IdFIFA TEXT, IsExtraTime INT2, MatchTime TIME, MatchType INTEGER,  MatchTypeName TEXT,  MatchGroup TEXT, Rank INTEGER, Stade INTEGER, StadeName TEXT, Team1 INTEGER, Team1Name TEXT,Team1IdFIFA TEXT, Team1Goal INTEGER, Team1GoalsBy TEXT, Team1Penalty INTEGER, Team2 INTEGER, Team2Name TEXT,Team2IdFIFA TEXT, Team2Goal INTEGER, Team2GoalsBy TEXT, Team2Penalty INTEGER);")
-            tx.executeSql("CREATE TABLE IF NOT EXISTS Video(ID INTEGER unique, Date DATETIME, Title TEXT, VideoLink TEXT, IdFIFA TEXT);")
-            tx.executeSql("CREATE TABLE IF NOT EXISTS News(ID TEXT unique, Date DATETIME, Title TEXT, Content TEXT, IdFIFA TEXT);")
+            tx.executeSql("CREATE TABLE IF NOT EXISTS Match(ID INTEGER unique, Date DATETIME, Hour TIME, IdFIFA TEXT, IsExtraTime INT2, MatchTime TIME, MatchType INTEGER,  MatchTypeName TEXT,  MatchGroup TEXT, Rank INTEGER, Stade INTEGER, StadeName TEXT, Team1 INTEGER, Team1Name TEXT,Team1IdFIFA TEXT, Team1Goal INTEGER, Team1GoalsBy TEXT, Team1Penalty INTEGER, Team2 INTEGER, Team2Name TEXT,Team2IdFIFA TEXT, Team2Goal INTEGER, Team2GoalsBy TEXT, Team2Penalty INTEGER, Broadcaster INTEGER);")
+            tx.executeSql("CREATE TABLE IF NOT EXISTS Video(ID INTEGER unique, Date DATETIME, Title TEXT, VideoLink TEXT, IdFIFA TEXT, ThumbLink TEXT);")
+            tx.executeSql("CREATE TABLE IF NOT EXISTS News(ID TEXT unique, Date DATETIME, Title TEXT, Summary TEXT, Content TEXT, IdFIFA TEXT, ThumbUrl TEXT);")
             tx.executeSql("CREATE TABLE IF NOT EXISTS MatchType(ID INTEGER unique, Libelle TEXT);")
             tx.executeSql("CREATE TABLE IF NOT EXISTS [Group](ID INTEGER unique, Libelle TEXT);")
             tx.executeSql("CREATE TABLE IF NOT EXISTS GroupResult(ID INTEGER unique, Libelle TEXT, [Group] INTEGER, GroupName TEXT, G INTEGER, P INTEGER, N INTEGER, BC INTEGER, BP INTEGER, PTS INTEGER);")
