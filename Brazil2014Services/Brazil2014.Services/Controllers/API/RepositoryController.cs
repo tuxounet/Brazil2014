b255f15862ef76b6d10e59506bce8fd6cf482f4a
Krux <krux@krux.fr>
2014-06-17 19:41:10 +0200

    Mise en place d'une méthode de syncronisation selective de données

--- Brazil2014Services/Brazil2014.Services/Controllers/API/RepositoryController.cs
+++ Brazil2014Services/Brazil2014.Services/Controllers/API/RepositoryController.cs
@@ -41,6 +41,54 @@ namespace C2S.Brazil2014.Services.Controllers.API
 
 
 
+        public CurrentRepository GetForItem(string entityName)
+        {
+
+            var l_ret = new CurrentRepository();
+
+            //Alimentation sélective des données du référentiel
+            switch (entityName)
+            {
+                case "Teams":
+                    l_ret.Teams = db.Team.OrderBy(p => p.ID).ToArray().Select(p => BTeam.FromEntity(p));
+                    break;
+
+                case "Stades":
+                    l_ret.Stades = db.Stade.OrderBy(p => p.ID).ToArray().Select(p => BStade.FromEntity(p));
+                    break;
+                case "Matchs":
+                    l_ret.Matchs = db.Match.OrderBy(p => p.ID).ToArray().Select(p => BMatch.FromEntity(p));
+                    break;
+                case "MatchTypes":
+                    l_ret.MatchTypes = db.MatchType.OrderBy(p => p.ID).ToArray().Select(p => BMatchType.FromEntity(p));
+                    break;
+                case "Videos":
+                    l_ret.Videos = db.Videos.OrderBy(p => p.Id).ToArray().Select(p => BVideo.FromEntity(p));
+                    break;
+                case "News":
+                    l_ret.News = db.News.OrderBy(p => p.Id).ToArray().Select(p => BNews.FromEntity(p));
+                    break;
+
+                case "Groups":
+                    l_ret.Groups = db.Group.OrderBy(p => p.ID).ToArray().Select(p => BGroup.FromEntity(p));
+                    break;
+                case "GroupResults":
+                    var gResults = new List<BTeamResultGroup>();
+                    foreach (var group in db.Group)
+                    {
+                        gResults.AddRange(db.Get_TeamResultGroup(group.Libelle).Select(p => BTeamResultGroup.FromEntity(group, p)));
+                    }
+                    l_ret.GroupResults = gResults;
+                    break;
+
+            }
+
+            return l_ret;
+        }
+
+
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
